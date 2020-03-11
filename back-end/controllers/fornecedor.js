d799fdadab18704dc021f05af0b8dd25d466a248
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-03-11 11:21:49 -0300

    (11/03) Termino do primeiro controller com excluir() e bysca()

--- back-end/controllers/fornecedor.js
+++ back-end/controllers/fornecedor.js
@@ -16,14 +16,22 @@ controller.novo = async (req, res) => {
 }
 
 controller.listar = async (req, res) => {
-   try {
-      // find() sem parâmetros: retorna todos
-      const lista = await Fornecedor.find()
-      res.send(lista) // O status HTTP 200 (OK) é implícito
+   
+   // Se houver query string, desvia para busca personalizada
+   if(Object.keys(req.query).length > 0) {
+      busca(req, res)
    }
-   catch(erro) {
-      console.log(erro)
-      res.status(500).send(erro)
+   else {
+   
+      try {
+         // find() sem parâmetros: retorna todos
+         const lista = await Fornecedor.find()
+         res.send(lista) // O status HTTP 200 (OK) é implícito
+      }
+      catch(erro) {
+         console.log(erro)
+         res.status(500).send(erro)
+      }
    }
 }
 
@@ -61,6 +69,45 @@ controller.atualizar = async (req, res) => {
       console.log(erro)
       res.status(500).send(erro)
    }
+}
+
+controller.excluir = async (req, res) => {
+   try {
+      const id = req.body._id
+      const obj = await Fornecedor.findByIdAndDelete(id)
+      if(obj) {
+         res.status(204).end()
+      }
+      else {
+         res.status(404).end()
+      }
+   }
+   catch(erro) {
+      console.log(erro)
+      res.status(500).send(erro)
+   }
+}
+
+async function busca (req, res) {
+   
+   let criterio = {}
+   let atrib = Object.keys(req.query)[0]
+   let valor = Object.values(req.query)[0]
+   
+   // /i no final da expressão regular significa que a
+   // busca será case insensitive
+   criterio[atrib] = { $regex: valor, $options: 'i' }
+   console.log(criterio)
+   
+   try{
+      let lista = await Fornecedor.find(criterio)
+      res.send(lista)
+   }
+   catch(erro) {
+      console.log(erro)
+      res.status(500).send(erro)
+   }
+
 }
 
 module.exports = controller
--- back-end/routes/fornecedor.js
+++ back-end/routes/fornecedor.js
@@ -7,5 +7,6 @@ router.post('/', controller.novo)
 router.get('/', controller.listar)
 router.get('/:id', controller.obterUm)
 router.put('/', controller.atualizar)
+router.delete('/', controller.excluir)
 
 module.exports = router
